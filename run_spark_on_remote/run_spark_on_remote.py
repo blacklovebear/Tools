e63421888bedc419f70f12cbe77340b690705b23
Zhou Peng <zhoupeng@tospur.com>
2017-03-08 14:04:57 +0800

    add spark --jar argv support

--- run_spark_on_remote/run_spark_on_remote.py
+++ run_spark_on_remote/run_spark_on_remote.py
@@ -11,12 +11,12 @@ def handle_first_disk_name(input_path):
     return '/'.join(path_list)
 
 
-def copy_spark_jar_to_remote(local_spark_jar_path, remote_ip):
-    handle_path = handle_first_disk_name(local_spark_jar_path)
+def copy_spark_jar_to_remote(remote_ip, local_jar_path):
+    handle_path = handle_first_disk_name(local_jar_path)
     call(["scp", handle_path, "root@%s:/home/spark/" % remote_ip])
 
 
-def run_shell_command_on_remote(remote_ip, local_spark_jar_path, spark_main_class, spark_argv):
+def run_shell_command_on_remote(remote_ip, local_jar_path, spark_run_command):
     ssh = paramiko.SSHClient()
     # ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -25,9 +25,13 @@ def run_shell_command_on_remote(remote_ip, local_spark_jar_path, spark_main_clas
     transport = ssh.get_transport()
     channel = transport.open_session()
 
-    jar_name = get_jar_name(local_spark_jar_path)
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(
-        "/usr/bin/spark-submit --master yarn-client --class %s /home/spark/%s %s" % (spark_main_class, jar_name, spark_argv), get_pty=True)
+    jar_name = get_jar_name(local_jar_path)
+    
+    # 莫名其妙的原因，命令行会在 /usr/bin/spark-submit等命令前面添加如下前缀
+    final_spark_run_command = spark_run_command.replace("C:/Program Files/Git", '')
+
+    print(final_spark_run_command)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(final_spark_run_command, get_pty=True)
 
     try:
         for line in iter(lambda:ssh_stdout.readline(64), ""):
@@ -44,24 +48,19 @@ def get_jar_name(jar_path):
     return jar_name
 
 
-def main(remote_ip, spark_main_class, local_spark_jar_path, argv):
-    copy_spark_jar_to_remote(local_spark_jar_path, remote_ip)
+def main(remote_ip, local_jar_path, spark_run_argv):
+    copy_spark_jar_to_remote(remote_ip, local_jar_path)
 
-    # spark run argv
-    if len(argv) > 4:
-        spark_argv = ' '.join( argv[4:] )
-    else:
-        spark_argv = ''
-
-    run_shell_command_on_remote(remote_ip, local_spark_jar_path, spark_main_class, spark_argv)
+    run_shell_command_on_remote(remote_ip, local_jar_path, ' '.join(spark_run_argv) )
 
 
 if __name__ == "__main__":
+    print(sys.argv)
     if len(sys.argv) >= 4:
-        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv)
+        main(sys.argv[1], sys.argv[2], sys.argv[3:])
     else:
-        print("Usage: python run_spark_on_remote.py remote_ip spark_class local_jar_path spak-run-argv")
-        print("eg: python run_spark_on_remote.py ip_address com.exmind.spark.UpdateMacHomeConsumeCycle e:/IdeaProjects/SparkProject/out/artifacts/SparkProject_jar/SparkProject.jar")
+        print("Usage: python run_spark_on_remote.py remote_ip local_jar_path spak-run-command")
+        # print("eg: python run_spark_on_remote.py ip_address com.exmind.spark.UpdateMacHomeConsumeCycle e:/IdeaProjects/SparkProject/out/artifacts/SparkProject_jar/SparkProject.jar")
 
 
     
